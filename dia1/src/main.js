dadd0bfee5e573dce41d5d728c9b0fd15bd4b8f7
guivictorr <guilhermevictor20@gmail.com>
2021-08-17 11:17:56 -0300

    solução dos primeiros exercícios

--- dia1/src/main.js
+++ dia1/src/main.js
@@ -1,6 +1,14 @@
 import './style.css'
 
-document.querySelector('#app').innerHTML = `
+const hideButton =  document.querySelector('[data-js="link"]')
+const app = document.querySelector('[data-js="app"]')
+
+app.innerHTML = `
   <h1>B. Academy</h1>
   <p>Boas vindas à semana de pré-work para o Bootcamp em React.js 😁</p>
 `
+
+hideButton.addEventListener('click', () => {
+  app.classList.toggle('hide')
+  hideButton.innerHTML = app.classList.contains('hide') ? 'Mostrar' : 'Esconder'
+})
